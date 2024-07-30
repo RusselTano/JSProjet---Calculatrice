078a117a29b205d7fc0a8e5e760cb50fb1ad6393
Dylane <tanolamene@gmail.com>
2024-07-30 08:05:54 -0300

    Implementer les calcules

--- app.js
+++ app.js
@@ -1,42 +1,109 @@
 const calc = document.querySelector(".calc");
-const ecran = document.querySelector(".calc__ecran");
+const calculation = document.querySelector(".cacl__calculation");
+const result = document.querySelector(".cacl__result");
+const buttons = [...document.querySelectorAll(".calc__button")];
 
-const buttons = document.querySelectorAll(".calc__button");
-const operatorsRegex = /[+\-x/]/;
-let tab = [];
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    let key = e.target.innerText;
-    checkSymbols(key);
-  });
-});
+const clearBtn = document.querySelector("[data-action='ce']");
+clearBtn.addEventListener("click", clear);
 
-function checkSymbols(key) {
-  if(key === "=") return;
-  if (key.toLocaleLowerCase() === "c") {
-    // Efface tout
-    tab.length = 0;
-    ecran.innerText = tab.join("");
+function clear() {
+  if (calculation.textContent[0] === "0") return;
+  else if (calculation.textContent.length === 1) newValue = "0";
+  else newValue = calculation.textContent.slice(0, -1);
 
-  } else if (key.toLocaleLowerCase() === "ce") {
-    // Efface le dernier élément
-    tab.pop();
-    ecran.innerText = tab.join("");
-
-  } else if (operatorsRegex.test(key)) {
-    // Si la touche est un opérateur
-    if (tab.length === 0 || operatorsRegex.test(tab[tab.length - 1])) {
-      // Si le tableau est vide ou le dernier élément est déjà un opérateur, on ne fait rien
-      console.log("Impossible d'ajouter un opérateur après un autre opérateur");
-    } else {
-      tab.push(key);
-      ecran.innerText = tab.join("");
-    }
-  } else {
-    // Ajouter le symbole et mettre à jour l'écran
-    tab.push(key);
-    ecran.innerText = tab.join('');
-  }
+  calculation.textContent = newValue;
 }
 
-function checkOperator(key) {}
+const resetBtn = document.querySelector("[data-action='c']");
+resetBtn.addEventListener("click", reset);
+
+function reset() {
+  calculation.textContent = "0";
+  result.textContent = "";
+}
+
+const digitsBtns = buttons.filter((button) =>
+  /[0-9]/.test(button.getAttribute("data-action"))
+);
+digitsBtns.forEach((digitBtn) => {
+  digitBtn.addEventListener("click", handleDigit);
+});
+
+function handleDigit(e) {
+  const btnValue = e.target.getAttribute("data-action");
+  console.log(btnValue);
+  if (btnValue === "0" && calculation.textContent === "0")
+    calculation.textContent = "";
+  else if (btnValue !== "0" && calculation.textContent === "0")
+    calculation.textContent = btnValue;
+  else calculation.textContent += btnValue;
+}
+
+const operatorsBtns = buttons.filter((button) =>
+  /[\/*+-]/.test(button.getAttribute("data-action"))
+);
+operatorsBtns.forEach((btn) => btn.addEventListener("click", handleOperators));
+
+function handleOperators(e) {
+  const btnValue = e.target.getAttribute("data-action");
+  calculation.textContent += btnValue;
+}
+console.log(digitsBtns, operatorsBtns);
+
+function extraireOperateurEtOperandes(expression) {
+  // Trouver l'opérateur
+  const operateurRegex = /[+\-*/]/;
+  const operateur = expression.match(operateurRegex);
+
+  // Séparer les opérandes en utilisant l'opérateur
+  const operandes = expression.split(operateur);
+
+  // Convertir les opérandes en nombres
+  const operande1 = parseFloat(operandes[0]);
+  const operande2 = parseFloat(operandes[1]);
+
+  return {
+    operande1,
+    operande2,
+    operateur: operateur[0],
+  };
+}
+
+const equalBtn = document.querySelector("[data-action='=']");
+equalBtn.addEventListener("click", showResult);
+
+function showResult() {
+  const calculationData = extraireOperateurEtOperandes(calculation.textContent);
+  result.textContent = calculer(
+    calculationData.operande1,
+    calculationData.operande2,
+    calculationData.operateur
+  );
+}
+
+function calculer(operande1, operande2, operateur) {
+  let resultat;
+
+  switch (operateur) {
+    case "+":
+      resultat = operande1 + operande2;
+      break;
+    case "-":
+      resultat = operande1 - operande2;
+      break;
+    case "*":
+      resultat = operande1 * operande2;
+      break;
+    case "/":
+      if (operande2 !== 0) {
+        resultat = operande1 / operande2;
+      } else {
+        return "Erreur : division par zéro";
+      }
+      break;
+    default:
+      return "Erreur : opérateur non valide";
+  }
+
+  return resultat;
+}
